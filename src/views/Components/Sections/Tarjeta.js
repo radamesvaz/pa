a289beda1780579d47fea9f8f7bc0bca2540d742
Radames Vaz <rgvtovar@hotmail.com>
2020-04-06 12:21:42 -0400

    fixing link

--- src/views/Components/Sections/Tarjeta.js
+++ src/views/Components/Sections/Tarjeta.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Tarjeta = ({ nombre, modelo, precio, link, url, id }) => {
     return (
         <div className = 'tc bg-light-purple white dib br3 pa3 ma2 grow bw2 shadow-5 relative'>
-        <a href={link}>
+        <a target="_blank" href={link}>
             <img alt='equipos' style={{height: '200px'}} src={url}/>
         </a>
             <div>
